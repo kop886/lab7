afd1726d54752692c3a0ad7050cd230722443ec8
Atamas Roman <atamas2702@gmail.com>
2024-11-03 12:10:09 +0200

    add new file

---
+++ ConsoleApplication23.cpp
@@ -0,0 +1,19 @@
+﻿#include <math.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    float x, Z, dob;
+    cout << "Enner x: ";
+    cin >> x;
+    dob = 1;
+    if (x < 0) {
+        cout << "The root of a negative number is impossible" << endl;
+        return 1;
+    }
+    for (int i = 1; i <= 5; i++) {
+        dob *= tan(i * x);
+    }
+    Z = dob + sqrt(x);
+    cout << "the result is " << Z << endl;
+}
